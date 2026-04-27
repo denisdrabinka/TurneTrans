46a4d5ced7e042643f57e5d06b28aabb69dfd0fe
Денис Драбинка <denisdrabinka@mail.ru>
2026-04-27 06:03:28 +0200

    **

--- TurneTrans/MainPage.xaml.cs
+++ TurneTrans/MainPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace TurneTrans
+﻿using System.Net;
+
+namespace TurneTrans
 {
     public partial class MainPage : ContentPage
     {
@@ -8,22 +10,23 @@
         public MainPage()
         {
             InitializeComponent();
+            var cookies = new CookieContainer();
+            var uri = new Uri("https://ext-dev.online.turne-trans.ru");
+            cookies.Add(new Cookie { Name = "login", Value = "schetDrevnihShizov@shueppsh.pulushka", Domain = uri.Host, Path = "/" });
+            cookies.Add(new Cookie { Name = "password", Value = "Nmwp7TGB", Domain = uri.Host, Path = "/" });
+
+            myWebView.Cookies = cookies;
+            myWebView.Source = new UrlWebViewSource { Url = "https://ext-dev.online.turne-trans.ru" };
+
             if (Preferences.ContainsKey("DeviceToken"))
             {
                 _deviceToken = Preferences.Get("DeviceToken", "");
             }
         }
 
-        private void OnCounterClicked(object? sender, EventArgs e)
+        private void OnNavigating(object sender, WebNavigatingEventArgs e)
         {
-            count++;
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
         }
     }
 }
